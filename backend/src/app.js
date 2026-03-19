a593d30105846bbd4c3c0f922e4834c3b019f62b
Max_Pdn <padonoumaxson@gmail.com>
2026-03-19 14:32:05 +0100

    initial

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import router from './routes/index.js';
+import 'dotenv/config';
+
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+
+app.use("/api", router);
+
+export default app;
---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import app from "./app.js";
+import cors from "cors";
+import { connectDB } from "./db/connect.js";
+
+
+const port = process.env.PORT ?? 3000;
+
+app.use(cors());
+await connectDB();
+
+app.listen(port, () => {
+  console.log(`✅L'application express a démarré sur le port ${port}`);
+});
