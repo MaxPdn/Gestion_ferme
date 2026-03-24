9868e98c44e00126a2d7a757ce688c48a5d3eeef
Marcel <agueroyessia@gmail.com>
2026-03-24 21:02:07 +0100

    alimentation 0%

---
+++ frontend/src/views/AlimentsListe.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import { ref } from 'vue';
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-2xl font-bold mb-4">Liste des Aliments</h1>
+    </div>
+</template>
+
+
+<style scoped></style>
