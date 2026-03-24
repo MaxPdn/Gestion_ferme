0af7dbceb3a82c97da49d729319b98053b247787
Kirchinnot <armelkirchinnot@gmail.com>
2026-03-24 16:28:55 +0100

    commit 35%

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,14 @@ import CampaignList from "../views/CampaignList.vue";
 import CampaignCreate from "../views/CampaignCreate.vue";
 import CampaignDetail from "../views/CampaignDetail.vue";
 import Departements from "@/views/Departements.vue";
+import Alert from "@/views/Alert.vue";
 
 const routes = [
   { path: "/campaigns", component: CampaignList },
   { path: "/create", component: CampaignCreate },
   { path: "/campaign/:id", component: CampaignDetail },
   { path: "/departements", component: Departements },
+  { path: "/alerts", component: Alert },
 ];
 
 export default createRouter({
